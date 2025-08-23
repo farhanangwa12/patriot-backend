f457678f5202555d7a3bf5e1efbff0323efe9215
farhanang <75283346+farhanangwa12@users.noreply.github.com>
2025-08-23 17:15:13 +0700

    feat: Update server to listen on BASE_URL environment variable

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 import app from './src/app.js';
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> {
+app.listen(PORT, process.env.BASE_URL ,()=> {
     console.log(`✅ Server running at http://localhost:${PORT}`);
 });
